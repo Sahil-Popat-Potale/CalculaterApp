b77d2e5657ac02ba69b53a4ddd05bb43a4511eb6
SAHIL-PC <sahilpopat97@gmail.com>
2023-04-26 13:18:41 +0530

    Done

---
+++ app/src/main/java/sahil/popat/calculaterapp/MainActivity.java
@@ -0,0 +1,64 @@
+package sahil.popat.calculaterapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    EditText editText1, editText2;
+    Button btn1, btn2, btn3, btn4, btn5;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        allocate();
+        sum();
+        sub();
+        mul();
+        div();
+        mode();
+    }
+    private void allocate() {
+        textView=findViewById(R.id.textView3);
+        editText1=findViewById(R.id.input1);
+        editText2=findViewById(R.id.input2);
+        btn1=findViewById(R.id.b1);
+        btn2=findViewById(R.id.b2);
+        btn3=findViewById(R.id.b3);
+        btn4=findViewById(R.id.b4);
+        btn5=findViewById(R.id.b5);
+    }
+    private void sum() { btn1.setOnClickListener(v -> {
+        int a=Integer.parseInt(editText1.getText().toString());
+        int b=Integer.parseInt(editText2.getText().toString());
+        int c=a+b;
+        textView.setText("Addition "+c);
+    });}
+    private void sub() { btn2.setOnClickListener(v -> {
+        int a=Integer.parseInt(editText1.getText().toString());
+        int b=Integer.parseInt(editText2.getText().toString());
+        int c=a-b;
+        textView.setText("Subtraction "+c);
+    });}
+    private void mul() {btn3.setOnClickListener(v -> {
+        int a=Integer.parseInt(editText1.getText().toString());
+        int b=Integer.parseInt(editText2.getText().toString());
+        int c=a*b;
+        textView.setText("Multiplication "+c);
+    });}
+    private void div() {btn4.setOnClickListener(v -> {
+        float a=Integer.parseInt(editText1.getText().toString());
+        float b=Integer.parseInt(editText2.getText().toString());
+        float c=a/b;
+        textView.setText("Division "+c);
+    });}
+    private void mode() {btn5.setOnClickListener(v -> {
+        float a=Integer.parseInt(editText1.getText().toString());
+        float b=Integer.parseInt(editText2.getText().toString());
+        float c=a%b;
+        textView.setText("Mode "+c);
+    });
+    }
+}
