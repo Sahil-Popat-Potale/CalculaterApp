e66db1f3a2f3f0b4f80a7cf086669cfa3cf7e9f9
SAHIL-PC <sahilpopat97@gmail.com>
2023-04-29 21:45:06 +0530

    minor changes

--- app/src/main/java/sahil/popat/calculaterapp/MainActivity.java
+++ app/src/main/java/sahil/popat/calculaterapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
     TextView textView;
     EditText editText1, editText2;
@@ -31,34 +33,62 @@ public class MainActivity extends AppCompatActivity {
         btn5=findViewById(R.id.b5);
     }
     private void sum() { btn1.setOnClickListener(v -> {
-        int a=Integer.parseInt(editText1.getText().toString());
-        int b=Integer.parseInt(editText2.getText().toString());
-        int c=a+b;
-        textView.setText("Addition  "+c);
-    });}
+            try {
+                int a = Integer.parseInt(editText1.getText().toString());
+                int b = Integer.parseInt(editText2.getText().toString());
+                int c = a + b;
+                textView.setText("Addition  " + c);
+            } catch (NumberFormatException e) {
+                Toast.makeText(getApplicationContext(), "Input field is empty or too long", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
     private void sub() { btn2.setOnClickListener(v -> {
-        int a=Integer.parseInt(editText1.getText().toString());
-        int b=Integer.parseInt(editText2.getText().toString());
-        int c=a-b;
-        textView.setText("Subtraction  "+c);
-    });}
-    private void mul() {btn3.setOnClickListener(v -> {
-        int a=Integer.parseInt(editText1.getText().toString());
-        int b=Integer.parseInt(editText2.getText().toString());
-        int c=a*b;
-        textView.setText("Multiplication  "+c);
-    });}
-    private void div() {btn4.setOnClickListener(v -> {
-        float a=Integer.parseInt(editText1.getText().toString());
-        float b=Integer.parseInt(editText2.getText().toString());
-        float c=a/b;
-        textView.setText("Division  "+c);
-    });}
-    private void module() {btn5.setOnClickListener(v -> {
-        float a=Integer.parseInt(editText1.getText().toString());
-        float b=Integer.parseInt(editText2.getText().toString());
-        float c=a%b;
-        textView.setText("Module  "+c);
-    });
+            try {
+                int a = Integer.parseInt(editText1.getText().toString());
+                int b = Integer.parseInt(editText2.getText().toString());
+                int c = a - b;
+                textView.setText("Subtraction  " + c);
+            } catch (NumberFormatException e) {
+                Toast.makeText(getApplicationContext(), "Input field is empty or too long", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+    private void mul() { btn3.setOnClickListener(v -> {
+            try {
+                int a = Integer.parseInt(editText1.getText().toString());
+                int b = Integer.parseInt(editText2.getText().toString());
+                int c = a * b;
+                textView.setText("Multiplication  " + c);
+            } catch (NumberFormatException e) {
+                Toast.makeText(getApplicationContext(), "Input field is empty or too long", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+    private void div() { btn4.setOnClickListener(v -> {
+            try {
+                float a = Float.parseFloat(editText1.getText().toString());
+                float b = Float.parseFloat(editText2.getText().toString());
+                float c = a / b;
+                textView.setText("Division  " + c);
+            } catch (NumberFormatException e) {
+                Toast.makeText(getApplicationContext(), "Input field is empty or too long", Toast.LENGTH_LONG).show();
+            } catch (ArithmeticException e) {
+                Toast.makeText(getApplicationContext(), "Cannot divide by zero", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void module() { btn5.setOnClickListener(v -> {
+            try {
+                float a = Float.parseFloat(editText1.getText().toString());
+                float b = Float.parseFloat(editText2.getText().toString());
+                float c = a % b;
+                textView.setText("Module  " + c);
+            } catch (NumberFormatException e) {
+                Toast.makeText(getApplicationContext(), "Input field is empty or too long", Toast.LENGTH_LONG).show();
+            } catch (ArithmeticException e) {
+                Toast.makeText(getApplicationContext(), "Cannot calculate module with zero divisor", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
